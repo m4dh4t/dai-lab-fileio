3ff9e46ee05927c2892bf15b308ebe015f65c5d6
m4dh4t <yann.pellegrin@heig-vd.ch>
2023-09-21 13:58:25 +0200

    Implement EncodingSelector for m4dh4t

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/m4dh4t/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/m4dh4t/EncodingSelector.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.fileio.m4dh4t;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class EncodingSelector {
 
@@ -17,7 +18,21 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+        if (file == null) {
+            return null;
+        }
+
+        String fileName = file.getName();
+        if (fileName.endsWith(".utf8")) {
+            return StandardCharsets.UTF_8;
+        } else if (fileName.endsWith(".txt")) {
+            return StandardCharsets.US_ASCII;
+        } else if (fileName.endsWith(".utf16be")) {
+            return StandardCharsets.UTF_16BE;
+        } else if (fileName.endsWith(".utf16le")) {
+            return StandardCharsets.UTF_16LE;
+        } else {
+            return null;
+        }
     }
 }
